b802ed922dacf47705cc9d25ac2dc3849ed80035
mirjSolution <ricojohndatoon23@gmail.com>
2021-01-02 03:18:35 -0500

    Animated Bar Chart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import {
-  select,
-  line,
-  curveCardinal,
-  axisBottom,
-  axisRight,
-  scaleLinear,
-} from 'd3';
+import { select, axisBottom, axisRight, scaleLinear, scaleBand } from 'd3';
 
 function App() {
   const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
@@ -16,36 +9,36 @@ function App() {
   // will be called initially and on every data change
   useEffect(() => {
     const svg = select(svgRef.current);
-    const xScale = scaleLinear()
-      .domain([0, data.length - 1])
-      .range([0, 300]);
+    const xScale = scaleBand()
+      .domain(data.map((value, index) => index))
+      .range([0, 300])
+      .padding(0.5);
 
     const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
 
-    const xAxis = axisBottom(xScale)
-      .ticks(data.length)
-      .tickFormat((index) => index + 1);
+    const colorScale = scaleLinear()
+      .domain([75, 100, 150])
+      .range(['green', 'orange', 'red'])
+      .clamp(true);
+
+    const xAxis = axisBottom(xScale).ticks(data.length);
     svg.select('.x-axis').style('transform', 'translateY(150px)').call(xAxis);
 
     const yAxis = axisRight(yScale);
     svg.select('.y-axis').style('transform', 'translateX(300px)').call(yAxis);
 
-    // generated the 'd' attributes of a path element
-    const myLine = line()
-      .x((value, index) => xScale(index))
-      .y(yScale)
-      .curve(curveCardinal);
-
-    // renders a path element, and attaches
-    // the 'd' attribute from line generator above
     svg
-      .selectAll('.line')
-      .data([data])
-      .join('path')
-      .attr('class', 'line')
-      .attr('d', myLine)
-      .attr('fill', 'none')
-      .attr('stroke', 'blue');
+      .selectAll('.bar')
+      .data(data)
+      .join('rect')
+      .attr('class', 'bar')
+      .style('transform', 'scale(1, -1)')
+      .attr('x', (value, index) => xScale(index))
+      .attr('y', -150)
+      .attr('width', xScale.bandwidth())
+      .transition()
+      .attr('fill', colorScale)
+      .attr('height', (value) => 150 - yScale(value));
   }, [data]);
   return (
     <React.Fragment>
