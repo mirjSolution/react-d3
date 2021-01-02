b4afcaaf9e287ddf2e16edea86bace5856905170
mirjSolution <ricojohndatoon23@gmail.com>
2021-01-02 01:51:43 -0500

    D3 React Basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
+import { select } from 'd3';
 
 function App() {
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const svgRef = useRef();
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll('circle')
+      .data(data)
+      .join('circle')
+      .attr('r', (value) => value)
+      .attr('cx', (value) => value * 2)
+      .attr('cy', (value) => value * 2)
+      .attr('stroke', 'red');
+  }, [data]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={() => setData(data.map((value) => value + 5))}>
+        Update Data
+      </button>
+      <button onClick={() => setData(data.filter((value) => value < 35))}>
+        Filter Data
+      </button>
+    </React.Fragment>
   );
 }
 
